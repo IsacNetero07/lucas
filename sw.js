bb04667a873b0b1dae5516e9f9cb540e109f4c4a
IsacNetero07 <isacnpomper@gmail.com>
2026-08-20 21:38:08 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C="lucas-v3";const A=["./","./index.html","./style.css","./app.js","./manifest.json","./assets/icon.svg"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
